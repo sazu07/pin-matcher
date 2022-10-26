9f45984fa52149b41cd4274aead4412cfc971730
sazu07 <sazushazahn0@gmail.com>
2022-10-26 15:32:13 +0600

    update code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-
+const button = document.getElementById('sub_mit')
+button.setAttribute('disabled','')
 function checkPin(){
     let pin = Math.round(Math.random()*10000);
     
@@ -25,6 +26,8 @@ function generatePin(){
 document.getElementById('calculator').addEventListener('click', function(event){
     let number = event.target.innerText;
     let calc= document.getElementById('typed-number');
+
+    button.removeAttribute('disabled')
      if(isNaN(number)){
         if (number=='C') {
             calc.value='';
@@ -35,5 +38,29 @@ document.getElementById('calculator').addEventListener('click', function(event){
         let newCalc = previousClac + number;
          calc.value=newCalc
      }
+
+})
+
+document.getElementById('sub_mit').addEventListener('click', function(){
+
+    const matchPin = document.getElementById('show-pin').value;
+    console.log(matchPin)
+    const typePin = document.getElementById('typed-number').value;
+    console.log(typePin);
+
+    if (matchPin=='') {
+        let success =  document.getElementById('success');
+        success.style.display='none'
+        let fail = document.getElementById('fail');
+        fail.style.display='none'
+    }
+    if (matchPin==typePin) {
+        let success =  document.getElementById('success');
+        success.style.display='block'
+     
+    }else {
+        let fail = document.getElementById('fail');
+        fail.style.display='block'
+    }
 
 })
